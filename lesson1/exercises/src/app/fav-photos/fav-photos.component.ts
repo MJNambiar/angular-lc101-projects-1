93b3c9a130a6bff05b0cd122a8f6d972be706202
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-07-21 12:16:17 -0500

    lesson 1 done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://i.redd.it/shxfapp952e71.jpg';
+  image2 = 'https://media.istockphoto.com/photos/perfect-sky-and-ocean-picture-id467367026?k=20&m=467367026&s=612x612&w=0&h=HBytXxkz31s-ZQrTwH3pbCy51Ep0SRScSXkChSUhYWk=';
+  image3 = 'https://www.travelandleisure.com/thmb/XLF2sRvYM2Q3y6GzlFR90tcC3kM=/1800x1200/filters:fill(auto,1)/header-DOLOMITESTG0422-fbe3eee35f6347f895af184866d0523d.jpg';
 
   constructor() { }
 
