5983c1da9c2591728bc7de499fefc37aab75c965
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-07-26 10:13:30 -0500

    examples for part 3 done

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,16 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactiveGold: boolean = false;
+   inactiveSilver: boolean = false;
+   inactiveCopper: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
 
-   // changeStatus (inactive: string): boolean {
-   //    if (inactive === "false") {
-   //       inactive === "true";
-   //    }
-   // }
+   changeStatus () {
+      this.inactiveGold =!this.inactiveGold;
+      this.inactiveSilver = !this.inactiveSilver;
+      this.inactiveCopper = !this.inactiveCopper;
+   }
 
 }
+
+
+// <!-- const toggleState = 
+//     () => {
+//         this.copperActive = !this.copperActive;
+//         this.goldActive = !this.goldActive;
+//         this.silverActive = !this.silverActive;
+//     } -->
